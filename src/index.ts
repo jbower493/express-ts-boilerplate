e9e69e1f822c0c5b4b4ba9689876a6d71c9996f1
Jamie Bower <j_bower@hotmail.co.uk>
2026-04-07 21:11:53 +1000

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express from "express";
 
-const PORT = 3000;
+const PORT = 4000;
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.get("/", (req, res) => {
   return res.json({ message: "Hi", data: [] });
