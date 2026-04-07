d0a77c18f946edbb4ad868f473b72cea4947014c
Jamie Bower <j_bower@hotmail.co.uk>
2026-04-07 18:14:56 +1000

    chore: basic express finished

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-const message: string = "hello world";
+import express from "express";
 
-console.log(message);
+const PORT = 3000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hi", data: [] });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Not found" });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
